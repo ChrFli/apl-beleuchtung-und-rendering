4a097e619e8f01e00aa555a544f0243603f0a764
Christin Flischikowski <s87513@stud.htw-dresden.de>
2024-10-26 15:41:27 +0200

    Create movable dummy char
    Tests für beweglichen Charakter, dafür neue Szene für das Spiel und eine Test-Szene eingerichtet. Erste Steuerung des Dummys umgesetzt.

---
+++ scripts/char_dummy.gd
@@ -0,0 +1,26 @@
+extends CharacterBody3D
+
+const SPEED = 100.0
+const JUMP_VELOCITY = 50.0
+const GRAVITY = 50.0  # Erhöhe diesen Wert, um das Objekt schneller fallen zu lassen
+
+func _physics_process(delta: float) -> void:
+	# Add gravity to the velocity
+	if not is_on_floor():
+		velocity.y -= GRAVITY * delta  # Hier wird die Schwerkraft auf das Objekt angewendet
+
+	# Handle jump
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle movement/deceleration
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
