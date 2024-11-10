d2dd547745dd8f95718f1c8f3a0aa23736f120dd
LongDuc89 <herrfurth.duc.long@gmail.com>
2024-11-10 13:22:35 +0100

    Key hinzugefügt, türen lassen sich nurnoch mit Key öffnen

--- scripts/doubledoors.gd
+++ scripts/doubledoors.gd
@@ -5,12 +5,17 @@ var interactable= true
 @export var playeranimation:AnimationPlayer
 
 func interact():
-	if interactable ==true:
-		interactable = false
-		toggle = !toggle
-		if toggle ==false:
-			playeranimation.play("closedouble")
-		if toggle ==true:
-			playeranimation.play("opendouble")
-		await get_tree().create_timer(1.0,false).timeout
-		interactable=true
+	var player = get_tree().get_nodes_in_group("player")[0] 
+	if player and player._haskey():  # Check if player has the key
+		if interactable ==true:
+			interactable = false
+			toggle = !toggle
+			if toggle ==false:
+				playeranimation.play("closedouble")
+			if toggle ==true:
+				playeranimation.play("opendouble")
+			await get_tree().create_timer(1.0,false).timeout
+			interactable=true
+	else:
+		# Door can't be opened message
+		print("The door can't be opened because you don't have the key.")
