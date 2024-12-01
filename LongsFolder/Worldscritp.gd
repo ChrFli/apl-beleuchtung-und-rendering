d15906f3f479dc99f82364eb08c82a55d918e80e
LongDuc89 <herrfurth.duc.long@gmail.com>
2024-12-01 15:08:26 +0100

    Shader beispiele

--- LongsFolder/Worldscritp.gd
+++ LongsFolder/Worldscritp.gd
@@ -2,9 +2,13 @@ extends Node3D
 
 @onready  var hit_rect = $UI/ColorRect
 
+var mynode= preload("res://LongsFolder/zombie.tscn")
 
 func _ready():
 	pass
+	var instance = mynode.instantiate();
+	instance.player_path = get_node("Firstperson").get_path() 
+	add_child(instance)
 	
 	
 func _process(delta):
@@ -15,4 +19,8 @@ func _on_firstperson_player_hit() -> void:
 	hit_rect.visible= true
 	await get_tree().create_timer(0.2).timeout
 	hit_rect.visible=false
+	
+	
+func _when_enter_zone():
+	var esa = 100
 	
