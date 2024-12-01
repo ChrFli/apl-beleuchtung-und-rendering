50701e70980579a2e1d0b6b7881cfdffcfb912bc
Christin Flischikowski <s87513@stud.htw-dresden.de>
2024-12-01 15:14:33 +0100

    Major Changes - Sound and Menu

---
+++ LongsFolder/Worldscritp.gd
@@ -0,0 +1,26 @@
+extends Node3D
+
+@onready  var hit_rect = $UI/ColorRect
+
+var mynode= preload("res://LongsFolder/zombie.tscn")
+
+func _ready():
+	pass
+	var instance = mynode.instantiate();
+	instance.player_path = get_node("Firstperson").get_path() 
+	add_child(instance)
+	
+	
+func _process(delta):
+	pass
+	
+
+func _on_firstperson_player_hit() -> void:
+	hit_rect.visible= true
+	await get_tree().create_timer(0.5).timeout
+	hit_rect.visible=false
+	
+	
+func _when_enter_zone():
+	var esa = 100
+	
---
+++ controls.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/StartMenu.tscn")
+	
+
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
